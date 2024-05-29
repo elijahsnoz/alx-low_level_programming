567fd4f4e48cd4bdfed2e61c7889b394b43f6ef3
elijahsnoz <elijahsnoz@gmail.com>
2024-05-29 23:39:50 +0100

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,6 +3,7 @@
 Module to calculate the perimeter of an island in a grid.
 """
 
+
 def island_perimeter(grid):
     """
     Returns the perimeter of the island described in grid.
@@ -13,18 +14,19 @@ def island_perimeter(grid):
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
                 # Check all four possible directions
                 if i == 0 or grid[i-1][j] == 0:  # Up
                     perimeter += 1
-                if i == rows-1 or grid[i+1][j] == 0:  # Down
+                if i == rows - 1 or grid[i + 1][j] == 0:  # Down
                     perimeter += 1
-                if j == 0 or grid[i][j-1] == 0:  # Left
+                if j == 0 or grid[i][j - 1] == 0:  # Left
                     perimeter += 1
-                if j == cols-1 or grid[i][j+1] == 0:  # Right
+                if j == cols - 1 or grid[i][j + 1] == 0:  # Right
                     perimeter += 1
-    
+
     return perimeter
+
