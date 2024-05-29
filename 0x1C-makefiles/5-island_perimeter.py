10d6170cc5b0cdbc4a1783174c062523fac6147d
elijahsnoz <elijahsnoz@gmail.com>
2024-05-29 23:49:45 +0100

    island_perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""5-island_perimeter module"""
+"""Module to calculate the perimeter of an island in a grid."""
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
-    c = 0
-    length = len(grid) - 1
-    width = len(grid[0]) - 1
+    """Returns the perimeter of the island described in grid.
+    
+    :param grid: List of list of integers where 0 represents water and 1 represents land
+    :return: The perimeter of the island
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
 
-    for i, r in enumerate(grid):
-        for j, n in enumerate(r):
-            if n == 1:
-                if i == 0 or grid[i - 1][j] != 1:
-                    c += 1
-                if j == 0 or grid[i][j - 1] != 1:
-                    c += 1
-                if j == width or grid[i][j + 1] != 1:
-                    c += 1
-                if i == length or grid[i + 1][j] != 1:
-                    c += 1
-    return c
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:  # Check Up
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:  # Check Left
+                    perimeter += 1
+                if i == rows - 1 or grid[i + 1][j] == 0:  # Check Down
+                    perimeter += 1
+                if j == cols - 1 or grid[i][j + 1] == 0:  # Check Right
+                    perimeter += 1
+
+    return perimeter
 
